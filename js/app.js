351cf306b3dfe18b328230a2f305dd7f275b0bb4
labing15 <labing15>
2015-11-10 10:03:07 -0500

    Nuestra pagina de presentacion

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$("#iniciarAnimacion").click(function(){
+		animacion();
+	});
+	
+	$("#detenerAnimacion").click(function(){
+		$("img").stop();
+	});
+	
+	function animacion(){
+		$("img").animate({"left":"600px"});
+		$("img").animate({"top":"500px"});
+		$("img").hide(2000);
+		$("img").show(2000);
+		$("img").animate({"left":"150px"});
+		$("img").animate({"top":"200px"});
+	}
+});
